9ba3774dfe6e6a4be5432817689bcf43894f455e
Denis Borisov <mai.borisov.denis@gmail.com>
2021-10-10 12:18:45 +0300

    .

--- bool_hacker.py
+++ bool_hacker.py
@@ -1,13 +1,18 @@
-from inspect import signature
-from typing import Callable
-
-
 def f(a, b, c):
     return a and (b or c)
 
 
-def hack(function: Callable):
-    print(signature(function))
+def hack(function):
+    #    argcount = function.__code__.co_argcount
+    arguments = function.__code__.co_varnames
+    generate_table(arguments)
+    # print(arguments)
+
+
+def generate_table(arguments):
+    table = [[0] * (len(arguments) + 1) for i in range(2 ** len(arguments))]
+    for i in range(len(table)):
+        print(table[i])
 
 
 hack(f)

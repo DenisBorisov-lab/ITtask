2249914286faaa0f09fd2a211c2cd76a8028eab4
Denis Borisov <mai.borisov.denis@gmail.com>
2021-10-10 23:28:49 +0300

    .

--- bool_hacker.py
+++ bool_hacker.py
@@ -2,13 +2,36 @@ def f(a, b, c):
     return a and (b or c)
 
 
+def medium4(a, b, c, d, e, f):
+    return a and not b or c and not d and e and f
+
+
+def logic1(a, b, c):
+    return a and b and c or a and b and not c
+
+
+def logic2(a, b, c):
+    return (not a or b) and not (a or b) and (not a or c)
+
+
+def medium2(a, b, c, d, e):
+    return a and b or (b and not a) or (c and e) or (not c and e) or (c and not e) or (not c and not e)
+
+
+def medium3(a, b, c, d, e):
+    return (a and b) and (b and not a) or (c and e) and (not c and e) or (c and not e) or (not c and not e)
+
+
 def hack(function):
     arguments = function.__code__.co_varnames
     print("Таблица истинности: ")
     generate_truth_table(arguments, function)
     print()
     print("Совершенная дизъюнктивная нормальная функция: ")
-    generate_sdnf(arguments, function)
+    print(generate_sdnf(arguments, function))
+    print()
+    print("Упрощённая сднф: ")
+    generate_simplified_sdnf(arguments, function)
 
 
 def generate_truth_table(arguments, function):
@@ -60,7 +83,7 @@ def generate_sdnf(arguments, function):
                     term += "-" + str(table[0][j]) + " * "
             sdnf += term[:-3] + ") + "
     sdnf = sdnf[:-3]
-    print(sdnf)
+    return sdnf
 
 
 def generate_table(arguments, function):
@@ -86,11 +109,20 @@ def generate_table(arguments, function):
         list_of_arguments = []
         for j in range(len(table[i]) - 1):
             list_of_arguments.append(table[i][j])
-        table[i][len(arguments)] = function(*list_of_arguments)
+        table[i][len(arguments)] = 1 if function(*list_of_arguments) else 0
 
     table.insert(0, names)
 
     return table
+
+
+def generate_simplified_sdnf(arguments, function):
+    sdnf = generate_sdnf(arguments, function)
+    terms = sdnf.split(" + ")
+    print(terms)
+    for i in range(len(terms) - 1):
+        args = terms[i][1:-1].split(" * ")
+        print(args)
 
 
 hack(f)

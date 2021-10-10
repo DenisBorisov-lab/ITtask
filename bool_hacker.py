282e7fe2e27475086ab6c7e4e7551f8bfede7836
Denis Borisov <mai.borisov.denis@gmail.com>
2021-10-10 19:47:39 +0300

    добавил СДНФ

--- bool_hacker.py
+++ bool_hacker.py
@@ -3,49 +3,23 @@ def f(a, b, c):
 
 
 def hack(function):
-    #    argcount = function.__code__.co_argcount
     arguments = function.__code__.co_varnames
-    generate_table(arguments, function)
-    # print(arguments)
+    print("Таблица истинности: ")
+    generate_truth_table(arguments, function)
+    print()
+    print("Совершенная дизъюнктивная нормальная функция: ")
+    generate_sdnf(arguments, function)
 
 
-def generate_table(arguments, function):
+def generate_truth_table(arguments, function):
     # генерация таблциы
-    table = [[0] * (len(arguments) + 1) for i in range(2 ** len(arguments))]
-
-    # заполнение сторики и сименами переменных
-    names = []
-    for i in range(len(arguments)):
-        names.append(arguments[i])
-    names.append("F")
-
-    # алгоритм заполнения таблица
-    counter = 2 ** len(arguments) / 2
-    now = 0
-    for i in range(len(arguments)):
-        for j in range(len(table)):
-            if now >= 2 * counter:
-                now = 0
-            if now >= counter:
-                table[j][i] = 1
-            now += 1
-        counter /= 2
-        now = 0
-
-    for i in range(len(table)):
-        list_of_arguments = []
-        for j in range(len(table[i]) - 1):
-            list_of_arguments.append(table[i][j])
-        table[i][len(arguments)] = function(*list_of_arguments)
+    table = generate_table(arguments, function)
 
     # объявление сепараторов
 
     column_separator = "┃"
     corner_separator = '╋'
     row_separator = '━'
-
-    # вставка строки с имена переменных и функции
-    table.insert(0, names)
 
     row_separator_array = []
     for i in range(len(arguments) + 1):
@@ -70,6 +44,53 @@ def generate_table(arguments, function):
                     print(column_separator + str(table[i][j]), end="")
 
         print()
+
+
+def generate_sdnf(arguments, function):
+    sdnf = ""
+    table = generate_table(arguments, function)
+
+    for i in range(1, len(table)):
+        if table[i][len(table[i]) - 1] == 1:
+            term = "("
+            for j in range(len(table[i]) - 1):
+                if table[i][j] == 1:
+                    term += str(table[0][j]) + " * "
+                else:
+                    term += "-" + str(table[0][j]) + " * "
+            sdnf += term[:-3] + ") + "
+    sdnf = sdnf[:-3]
+    print(sdnf)
+
+
+def generate_table(arguments, function):
+    names = []
+    for i in range(len(arguments)):
+        names.append(arguments[i])
+    names.append("F")
+
+    table = [[0] * (len(arguments) + 1) for i in range(2 ** len(arguments))]
+    counter = 2 ** len(arguments) / 2
+    now = 0
+    for i in range(len(arguments)):
+        for j in range(len(table)):
+            if now >= 2 * counter:
+                now = 0
+            if now >= counter:
+                table[j][i] = 1
+            now += 1
+        counter /= 2
+        now = 0
+
+    for i in range(len(table)):
+        list_of_arguments = []
+        for j in range(len(table[i]) - 1):
+            list_of_arguments.append(table[i][j])
+        table[i][len(arguments)] = function(*list_of_arguments)
+
+    table.insert(0, names)
+
+    return table
 
 
 hack(f)
